1802ab1be0e983f7870d2556cb35487d3f74b4ed
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-08-28 16:23:57 +0800

    modify ci

--- .github/ci/build/build_ios_ipa.sh
+++ .github/ci/build/build_ios_ipa.sh
@@ -3,7 +3,7 @@ CURRENT_PATH=$PWD
 # 获取项目目录
 PROJECT_PATH="$( cd "$1" && pwd  )"
 
-cd ${PROJECT_PATH} && pod install
+cd ${PROJECT_PATH} && pod install --repo-update
 
 if [ $? -eq 0 ]; then
     echo "success"
